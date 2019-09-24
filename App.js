d61d84ec2f5c3d8e3a8c7d754ff23a1b3b90e730
Manassés <manavitorino@gmail.com>
2019-09-24 20:14:19 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -6,6 +6,22 @@ import SignUp from './src/pages/signup'
 import Login from './src/pages/login'
 import Main from './src/pages/main'
 
+import firebase from 'firebase'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCuoQrt6twCVld2GHTSu0Wn3hKGX0qkBgg",
+  authDomain: "notification-1b2fc.firebaseapp.com",
+  databaseURL: "https://notification-1b2fc.firebaseio.com",
+  projectId: "notification-1b2fc",
+  storageBucket: "notification-1b2fc.appspot.com",
+  messagingSenderId: "768391709712",
+  appId: "1:768391709712:web:4c67a05145c15b73d73695"
+};
+
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -25,6 +41,9 @@ const AppNavigator = createStackNavigator({
   },
   SignUp: {
     screen: SignUp,
+  },
+  Main: {
+    screen: Main
   },
 },
 {
